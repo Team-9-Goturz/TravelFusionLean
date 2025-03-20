e81f0c98ad08e5894a861d20bc730a24c5b6b790
Ea Overvad <ea@0vervad.dk>
2025-03-20 19:36:30 +0100

    Be right back

--- TravelFusionLean/TravelFusionLean/Data/AppDbContext.cs
+++ TravelFusionLean/TravelFusionLean/Data/AppDbContext.cs
@@ -6,6 +6,7 @@ namespace TravelFusionLean.Data;
 public class AppDbContext : DbContext
 {
     public DbSet<UserRole> UserRoles { get; set; }
+    public DbSet<User> Users { get; set; }
 
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
@@ -19,8 +20,19 @@ public class AppDbContext : DbContext
 
             entity.HasKey(ur => ur.Id);
             entity.Property(ur => ur.Name)
-                .IsRequired();
+               .IsRequired();
 
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("User");
+
+            entity.HasKey(u => u.id);
+            entity.Property(u => u.username).IsRequired();
+            entity.Property(u => u.passwordHash).IsRequired();
+            entity.Property(u => u.passwordSalt).IsRequired();
+            entity.Property(u => u.email).IsRequired(); 
         });
     }
 
---
+++ TravelFusionLean/TravelFusionLean/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace TravelFusionLean.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string passwordHash { get; set; }
+        public string passwordSalt { get; set; }
+
+        public string email { get; set; }
+
+        public UserRole UserRole { get; set; }
+
+
+    }
+}
