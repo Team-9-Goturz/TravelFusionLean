bfd9628a0b2fed5613b5d1a21190b90d153e95a8
Ea Overvad <ea@0vervad.dk>
2025-05-05 19:18:23 +0200

    opstart

--- ServiceImplementations/BookingService.cs
+++ ServiceImplementations/BookingService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,46 @@ namespace ServiceImplementations
                 .Include(b => b.Payment)
                 .ToListAsync(); // <-- Her!
 
+        }
+        public override async Task<Booking> GetByIdAsync(int id)
+        {
+            Booking booking = _context.Bookings
+                .Include(b => b.TravelPackage)
+                .FirstOrDefault(u => u.Id == id);
+            booking.Status = BookingStatus.Cancelled;
+            return await UpdateAsync(booking);
+        }
+        public async Task<Booking> CancelById(int id)
+        {
+            Booking booking = await this.GetByIdAsync(id);
+            booking.Status = BookingStatus.Cancelled;
+
+            return await this.UpdateAsync(booking);
+        }
+
+        public override async Task<bool> DeleteAsync(int id)
+        {
+            try
+            {
+                Booking booking = await GetByIdAsync(id);
+                if (booking == null) return false;
+
+                //Slet payment
+
+                _dbSet.Remove(booking);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (DbUpdateException dbEx)
+            {
+                Debug.WriteLine($"[DeleteAsync] DB fejl: {dbEx.Message}");
+                throw new ApplicationException("Kunne ikke slette entiteten fra databasen.", dbEx);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"[DeleteAsync] Ukendt fejl: {ex.Message}");
+                throw new ApplicationException("Uventet fejl ved sletning.", ex);
+            }
         }
     }
 }
