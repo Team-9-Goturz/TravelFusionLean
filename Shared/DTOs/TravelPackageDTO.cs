16b427d5f531ed1e9ba3035179adc546b54db322
Ea Overvad <ea@0vervad.dk>
2025-05-08 23:13:03 +0200

    Break

---
+++ Shared/DTOs/TravelPackageDTO.cs
@@ -0,0 +1,30 @@
+﻿using Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Dtos
+{
+    public class TravelPackageDTO
+    {
+        [StringLength(100)]
+        public string? Headline { get; set; }
+
+        public Price Price { get; set; }
+        public int NoOfTravellers { get; set; }
+
+        [StringLength(600)]
+        public string? Description { get; set; }
+
+        // Navigation properties
+        public Flight OutboundFlight { get; set; }
+        public Flight InboundFlight { get; set; }
+        public HotelStay HotelStay { get; set; }
+       
+        // For at uplade billeder, når man opretter travelpackages
+        public string? ImagePath { get; set; }
+    }
+}
