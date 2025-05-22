51c4a4365896804ae125dd534202fb115047f6d6
Ea Overvad <ea@0vervad.dk>
2025-05-22 22:43:23 +0200

    opdelte flyvælgerne i mindre razor komponenter for DRY og Single responsibility

---
+++ Shared/DTOs/FligthSearchDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DTOs
+{
+    public class FligthSearchDTO
+    {
+        public string? SearchFrom;
+        public string? SearchTo;
+        public DateTime? SearchDate;
+    }
+}
