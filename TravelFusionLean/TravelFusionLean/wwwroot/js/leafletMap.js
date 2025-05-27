525fcfae50ec7931fa20b7899ba39aaf196438c1
Ea Overvad <ea@0vervad.dk>
2025-05-27 10:39:28 +0200

    Added map

---
+++ TravelFusionLean/TravelFusionLean/wwwroot/js/leafletMap.js
@@ -0,0 +1,12 @@
+﻿export function initializeMap() {
+    const map = L.map('map').setView([56.4607, 10.0364], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap-bidragsydere'
+    }).addTo(map);
+
+    L.marker([56.4607, 10.0364])
+        .addTo(map)
+        .bindPopup("Hotel Randers")
+        .openPopup();
+}
