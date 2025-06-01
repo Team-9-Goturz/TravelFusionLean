8072707d05a77d26bc90b1809a828a8ea7ec9767
Ea Overvad <ea@0vervad.dk>
2025-06-01 10:26:52 +0200

    TravelPackage builder added

---
+++ ServiceImplementations/TravelPackageSearchBuilder.cs
@@ -0,0 +1,44 @@
+﻿using Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceImplementations
+{
+    public class TravelPackageSearchBuilder
+    {
+        private readonly TravelPackageSearchDTO _searchDto = new();
+
+        public TravelPackageSearchBuilder WithDepartureDateEarliest(DateOnly date)
+        {
+            _searchDto.DepartureDateEarliest = date;
+            return this;
+        }
+
+        public TravelPackageSearchBuilder WithDepartureDateLatest(DateOnly date)
+        {
+            _searchDto.DepartureDateLatest = date;
+            return this;
+        }
+
+        public TravelPackageSearchBuilder WithRecommendedOnly(bool recommended = true)
+        {
+            _searchDto.HasToBeRecommended = recommended;
+            return this;
+        }
+
+        public TravelPackageSearchBuilder WithDestination(string destination)
+        {
+            _searchDto.Destination = destination;
+            return this;
+        }
+
+        public TravelPackageSearchDTO Build()
+        {
+            return _searchDto;
+        }
+    }
+
+}
